1807f56a374c10289d3eedb1d56ec3a816414757
Stefan Dieterle <stefand@artcom.de>
2017-06-19 14:09:52 +0200

    Move from one to one to one to many relationship between point and position data models

--- src/models/point.js
+++ src/models/point.js
@@ -3,13 +3,13 @@ const db = require("../db")
 const PositionData = require("./positionData")
 
 const Point = db.define("point", {
-  pointId: { type: Sequelize.INTEGER },
+  pointId: { type: Sequelize.INTEGER, unique: true },
   trueCoordinateX: { type: Sequelize.FLOAT, allowNull: false },
   trueCoordinateY: { type: Sequelize.FLOAT, allowNull: false },
   trueCoordinateZ: { type: Sequelize.FLOAT, allowNull: false },
   trueRoomLabel: { type: Sequelize.STRING, allowNull: false },
 })
 
-Point.belongsTo(PositionData)
+Point.hasMany(PositionData, { sourceKey: "pointId" })
 
 module.exports = Point
