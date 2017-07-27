9ba7f08a8246c1f24368577c3a6b2090c5922b0c
Stefan Dieterle <stefand@artcom.de>
2017-07-27 16:10:20 +0200

    Add tests for storePositionData

---
+++ specs/storeData/storePositionData.spec.js
@@ -0,0 +1,35 @@
+const { describe, it, before, after } = require("mocha")
+const { dbSync, dbDrop } = require("../helpers/db")
+const Experiment = require("../../src/models/experiment")
+const Point = require("../../src/models/point")
+const points = require("../testData/points.json")
+const PositionData = require("../../src/models/positionData")
+const positionData = require("../testData/positionData.json")
+const storePositionData = require("../../src/storeData/storePositionData")
+const { checkPositionData } = require("../helpers/data")
+
+
+describe("Model PositionData", () => {
+  before((done) => {
+    dbSync().then(done).catch(done)
+  })
+
+  after((done) => {
+    dbDrop().then(done).catch(done)
+  })
+
+  describe("Model PositionData basic function", () => {
+    it("can create position data", done => {
+      Experiment.create({ name: "test-experiment" })
+        .then(() => Point.bulkCreate(points))
+        .then(() => {
+          storePositionData(positionData)
+            .then(() => PositionData.findAll())
+            .then(queryResults => {
+              checkPositionData(queryResults)
+              done()
+            }).catch(done)
+        })
+    })
+  })
+})
