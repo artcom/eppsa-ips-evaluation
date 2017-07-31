1664b1d73b65ef7a9d76fd07a2f5f913dd8b79ce
Stefan Dieterle <stefand@artcom.de>
2017-07-31 17:09:37 +0200

    Make main the server

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-const { QuuppaExperiment } = require("./runExperiment/quuppaExperiment")
+// const { QuuppaExperiment } = require("./runExperiment/quuppaExperiment")
 const server = require("./server")
+const { initializeDb } = require("./initializeDb")
 
-const quuppaExperiment = new QuuppaExperiment("test_quuppa")
-quuppaExperiment.run().catch(error => console.error(error))
+// const quuppaExperiment = new QuuppaExperiment("test_quuppa")
+// quuppaExperiment.run().catch(error => console.error(error))
 
-server.listen(3000, () => console.log("server listening on port 3000"))
+initializeDb().then(() => {
+  server.listen(3000, () => console.log("server listening on port 3000"))
+})
