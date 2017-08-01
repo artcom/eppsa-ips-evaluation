e6b49ce452047d0dd5451790155b058f568f354c
Stefan Dieterle <stefand@artcom.de>
2017-08-01 12:38:01 +0200

    Refactor

--- specs/server/points.spec.js
+++ specs/server/points.spec.js
@@ -9,55 +9,53 @@ const server = require("../../src/server")
 const { setUpPoints } = require("../../src/setUpExperiment")
 
 describe("Server for points", () => {
-  beforeEach((done) => {
-    dbSync().then(done).catch(done)
+  beforeEach(async () => {
+    await dbSync()
     this.server = server.listen(3000, () => console.log("server listening on port 3000"))
   })
 
-  afterEach((done) => {
-    dbDrop().then(done).catch(done)
+  afterEach(async () => {
+    await dbDrop()
     this.server.close()
   })
 
-  it("should return all points on get at /points", done => {
-    setUpPoints(points)
-    restler.get("http://localhost:3000/points").on("complete", (data, response) => {
+  it("should return all points on get at /points", async () => {
+    await setUpPoints(points)
+    await restler.get("http://localhost:3000/points").on("complete", (data, response) => {
       expect(response.statusCode).to.equal(200)
       expect(sortBy(data, ["name"])).to.deep.equal(sortBy(points, ["name"]))
-      done()
     })
   })
 
-  it("should return point data on get at /points/point-name", done => {
-    setUpPoints(points)
-    restler.get("http://localhost:3000/points/point1")
+  it("should return point data on get at /points/point-name", async () => {
+    await setUpPoints(points)
+    await restler.get("http://localhost:3000/points/point1")
       .on("complete", (data, response) => {
         expect(response.statusCode).to.equal(200)
         expect(data[0]).to.deep.equal(points[1])
-        done()
       })
   })
 
-  it("should return point name in body and path in location header on post at /points", done => {
-    restler.post("http://localhost:3000/points", {
-      data: points[1]
-    }).on("complete", (data, response) => {
-      expect(response.statusCode).to.equal(201)
-      expect(response.headers.location).to.equal("/points/point1")
-      expect(data).to.equal("point1")
-      done()
-    })
-  })
+  it("should return point name in body and path in location header on post at /points",
+    async () => {
+      await restler.post("http://localhost:3000/points", {
+        data: points[1]
+      }).on("complete", (data, response) => {
+        expect(response.statusCode).to.equal(201)
+        expect(response.headers.location).to.equal("/points/point1")
+        expect(data).to.equal("point1")
+      })
+    }
+  )
 
-  it("should store the point in the database on post at /points", done => {
-    restler.post("http://localhost:3000/points", {
+  it("should store the point in the database on post at /points", async () => {
+    await restler.post("http://localhost:3000/points", {
       data: points[1]
     }).on("complete", (data, response) => {
       expect(response.statusCode).to.equal(201)
       Point.findAll().then(storedPoints => {
         expect(pick(storedPoints[0], keys(points[1]))).to.deep.equal(points[1])
-        done()
-      }).catch(done)
+      })
     })
   })
 })
