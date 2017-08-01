b5fac50604d21b403d65783e877ab2d566983cc7
Stefan Dieterle <stefand@artcom.de>
2017-08-01 12:52:48 +0200

    Refactor

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ const { initializeDb } = require("./initializeDb")
 // const quuppaExperiment = new QuuppaExperiment("test_quuppa")
 // quuppaExperiment.run().catch(error => console.error(error))
 
-initializeDb().then(() => {
+async function runServer() {
+  await initializeDb()
   server.listen(3000, () => console.log("server listening on port 3000"))
-})
+}
+
+runServer()
