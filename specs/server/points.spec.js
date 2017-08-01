74f4b9bcd43e31aaa112f0a012caea9828bf1fd4
Stefan Dieterle <stefand@artcom.de>
2017-08-01 16:03:43 +0200

    Add points bulk creation end point

--- specs/server/points.spec.js
+++ specs/server/points.spec.js
@@ -40,7 +40,7 @@ describe("Server for points", () => {
     })
   })
 
-  it("should return point name in body and path in location header on post at /points",
+  it("should return point name in body and path in location header on single point post at /points",
     done => {
       restler.post("http://localhost:3000/points", {
         data: points[1]
@@ -53,13 +53,46 @@ describe("Server for points", () => {
     }
   )
 
-  it("should store the point in the database on post at /points", done => {
+  it("should store the point in the database on single point post at /points", done => {
     restler.post("http://localhost:3000/points", {
       data: points[1]
     }).on("complete", async (data, response) => {
       expect(response.statusCode).to.equal(201)
       const storedPoints = await Point.findAll()
       expect(pick(storedPoints[0], keys(points[1]))).to.deep.equal(points[1])
+      done()
+    })
+  })
+
+  it("should return point name in body and path in location header on multiple point post at " +
+    "/points/bulk",
+    done => {
+      restler.post("http://localhost:3000/points/bulk", {
+        data: points
+      }).on("complete", (data, response) => {
+        expect(response.statusCode).to.equal(201)
+        expect(response.headers.location).to.equal(
+          "/points/point0; /points/point1; /points/point2; /points/point3"
+        )
+        expect(data).to.deep.equal([
+          "point0",
+          "point1",
+          "point2",
+          "point3"
+        ])
+        done()
+      })
+    }
+  )
+
+  it("should store the point in the database on multiple point post at /points/bulk", done => {
+    restler.post("http://localhost:3000/points/bulk", {
+      data: points
+    }).on("complete", async (data, response) => {
+      expect(response.statusCode).to.equal(201)
+      const storedPointsQueryResult = await Point.findAll()
+      const storedPoints = storedPointsQueryResult.map(point => pick(point, keys(points[0])))
+      expect(storedPoints).to.deep.equal(points)
       done()
     })
   })
--- src/server/points.js
+++ src/server/points.js
@@ -1,6 +1,7 @@
 const multer = require("multer")
+const { keys } = require("lodash")
 const { getPoints, getPointsByName } = require("../getData")
-const { insertPoint } = require("../storeData")
+const { insertPoint, insertPoints } = require("../storeData")
 
 
 const upload = multer()
@@ -19,6 +20,16 @@ const servePoints = function servePoints(server) {
   server.post("/points", upload.array(), async (request, response) => {
     await insertPoint(request.body)
     response.append("location", `/points/${request.body.name}`).status(201).send(request.body.name)
+  })
+
+  server.post("/points/bulk", upload.array(), async (request, response) => {
+    const points = keys(request.body).map(key => request.body[key])
+    await insertPoints(points)
+
+    response
+      .append("location", points.map(point => `/points/${point.name}`).join("; "))
+      .status(201)
+      .send(points.map(point => point.name))
   })
 
   return server
