511f82cbcff4cc8b0f30d41bda65f1445adbb355
Stefan Dieterle <stefand@artcom.de>
2017-08-28 16:04:42 +0200

    Add unique constraint on zone name

--- specs/models/zones.spec.js
+++ specs/models/zones.spec.js
@@ -1,17 +1,17 @@
-const { describe, it, before, after } = require("mocha")
-const { expect } = require("chai")
-const { keys, pick, sortBy } = require("lodash")
+const { describe, it, beforeEach, afterEach } = require("mocha")
+const { expect, assert } = require("chai")
+const { keys, pick, sortBy, merge, omit } = require("lodash")
 const { dbSync, dbDrop } = require("../helpers/db")
 const Zone = require("../../src/models/zone")
 const zones = require("../testData/zones.json")
 
 
 describe("Model Zone", () => {
-  before(async () => {
+  beforeEach(async () => {
     await dbSync()
   })
 
-  after(async () => {
+  afterEach(async () => {
     await dbDrop()
   })
 
@@ -22,5 +22,19 @@ describe("Model Zone", () => {
       .map(queryResult => pick(queryResult, keys(zones[0])))
     expect(sortBy(storedZones, ["name"]))
       .to.deep.equal(sortBy(zones, ["name"]))
+  })
+
+  it("raises an error when the zone name already exists", async () => {
+    try {
+      await Zone.bulkCreate(
+        [
+          zones[0],
+          merge({ name: "zone1" }, omit(zones[1], "name"))
+        ]
+      )
+      assert.fail("", "Validation error", "No error was thrown")
+    } catch (error) {
+      expect(error.message).to.equal("Validation error")
+    }
   })
 })
