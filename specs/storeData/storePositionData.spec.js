d03d5476aa26e18114e15d3df57f02b2e5acc4f9
Stefan Dieterle <stefand@artcom.de>
2017-08-28 11:07:07 +0200

    Adapt storePosition test to PositionData relationship to Node

--- specs/storeData/storePositionData.spec.js
+++ specs/storeData/storePositionData.spec.js
@@ -1,6 +1,8 @@
 const { describe, it, before, after } = require("mocha")
 const { dbSync, dbDrop } = require("../helpers/db")
 const Experiment = require("../../src/models/experiment")
+const Node = require("../../src/models/node")
+const nodes = require("../testData/nodes.json")
 const Point = require("../../src/models/point")
 const points = require("../testData/points.json")
 const PositionData = require("../../src/models/positionData")
@@ -9,7 +11,7 @@ const storePositionData = require("../../src/storeData/storePositionData")
 const { checkPositionData } = require("../helpers/data")
 
 
-describe("Model PositionData", () => {
+describe("storePositionData", () => {
   before(async () => {
     await dbSync()
   })
@@ -18,13 +20,12 @@ describe("Model PositionData", () => {
     await dbDrop()
   })
 
-  describe("Model PositionData basic function", () => {
-    it("can create position data", async () => {
-      await Experiment.create({ name: "test-experiment" })
-      await Point.bulkCreate(points)
-      await storePositionData(positionData)
-      const queryResults = await PositionData.findAll()
-      checkPositionData(queryResults)
-    })
+  it("can create position data", async () => {
+    await Experiment.create({ name: "test-experiment" })
+    await Point.bulkCreate(points)
+    await Node.bulkCreate(nodes)
+    await storePositionData(positionData)
+    const queryResults = await PositionData.findAll()
+    checkPositionData(queryResults)
   })
 })
