b79c8bd96077803780978d4c81b25f37f5565c41
Stefan Dieterle <stefand@artcom.de>
2017-08-29 16:24:50 +0200

    Use restling instead of restler

--- specs/server/experiments.spec.js
+++ specs/server/experiments.spec.js
@@ -2,7 +2,6 @@ const { describe, it, beforeEach, afterEach } = require("mocha")
 const { expect } = require("chai")
 const proxyquire = require("proxyquire")
 const sinon = require("sinon")
-const restler = require("restler")
 const rest = require("restling")
 const Experiment = require("../../src/models/experiment")
 const { dbSync, dbDrop } = require("../helpers/db")
@@ -23,64 +22,54 @@ const zones = require("../testData/zones.json")
 describe("Server for experiments", () => {
   beforeEach(async () => {
     await dbSync()
-    this.getData = sinon.stub(getQuuppaData, "getQuuppaData").callsFake(getMockData)
-    this.runQuuppaExperiment = proxyquire(
-      "../../src/runExperiment/quuppaExperiment",
-      { getQuuppaData: { getQuuppaData: this.getData } }
-    )
     this.server = server.listen(3000, () => console.log("server listening on port 3000"))
   })
 
   afterEach(async () => {
     await dbDrop()
-    getQuuppaData.getQuuppaData.restore()
     this.server.close()
   })
 
   it("should return 200 at /", async () => {
-    restler.get("http://localhost:3000")
-      .on("complete", (data, response) => {
-        expect(response.statusCode).to.equal(200)
-        expect(data).to.equal("")
-      })
+    const result = await rest.get("http://localhost:3000")
+    expect(result.response.statusCode).to.equal(200)
+    expect(result.data).to.equal("")
   })
 
   it("should return all experiments on get at /experiments", async () => {
     await insertExperiment("test-experiment")
-    restler.get("http://localhost:3000/experiments")
-      .on("complete", (data, response) => {
-        expect(response.statusCode).to.equal(200)
-        expect(data).to.deep.equal([{ name: "test-experiment" }])
-      })
+    const result = await rest.get("http://localhost:3000/experiments")
+    expect(result.response.statusCode).to.equal(200)
+    expect(result.data).to.deep.equal([{ name: "test-experiment" }])
   })
 
   it("should return experiment name on get at /experiments/experiment-name", async () => {
     await insertExperiment("test-experiment")
-    restler.get("http://localhost:3000/experiments/test-experiment")
-      .on("complete", (data, response) => {
-        expect(response.statusCode).to.equal(200)
-        expect(data).to.deep.equal({ name: "test-experiment" })
-      })
+    const result = await rest.get("http://localhost:3000/experiments/test-experiment")
+    expect(result.response.statusCode).to.equal(200)
+    expect(result.data).to.deep.equal({ name: "test-experiment" })
   })
 
   it("should return experiment name in body and path in location header on post at /experiments",
       async () => {
-        restler.post("http://localhost:3000/experiments", { data: { name: "test-experiment" } })
-          .on("complete", (data, response) => {
-            expect(response.statusCode).to.equal(201)
-            expect(response.headers.location).to.equal("/experiments/test-experiment")
-            expect(data).to.equal("test-experiment")
-          })
+        const result = await rest.post(
+          "http://localhost:3000/experiments",
+          { data: { name: "test-experiment" } }
+        )
+        expect(result.response.statusCode).to.equal(201)
+        expect(result.response.headers.location).to.equal("/experiments/test-experiment")
+        expect(result.data).to.equal("test-experiment")
       }
   )
 
   it("should store the experiment in the database on post at /experiments", async () => {
-    restler.post("http://localhost:3000/experiments", { data: { name: "test-experiment" } })
-      .on("complete", async (data, response) => {
-        expect(response.statusCode).to.equal(201)
-        const experiments = await Experiment.findAll()
-        expect(experiments[0].name).to.equal("test-experiment")
-      })
+    const result = await rest.post(
+      "http://localhost:3000/experiments",
+      { data: { name: "test-experiment" } }
+    )
+    expect(result.response.statusCode).to.equal(201)
+    const experiments = await Experiment.findAll()
+    expect(experiments[0].name).to.equal("test-experiment")
   })
 })
 
