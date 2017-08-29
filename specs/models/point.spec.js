bf23af67cd1433694bc071b38a4375d76a6a4e0a
Stefan Dieterle <stefand@artcom.de>
2017-08-29 09:02:42 +0200

    Add tests for assigning zones to points upon storing points

--- specs/models/point.spec.js
+++ specs/models/point.spec.js
@@ -1,26 +1,44 @@
-const { describe, it, before, after } = require("mocha")
+const { describe, it, beforeEach, afterEach } = require("mocha")
 const { expect } = require("chai")
-const { keys, pick, sortBy } = require("lodash")
+const { assign, keys, pick, sortBy } = require("lodash")
 const { dbSync, dbDrop } = require("../helpers/db")
 const Point = require("../../src/models/point")
 const points = require("../testData/points.json")
+const Zone = require("../../src/models/zone")
+const zones = require("../testData/zones.json")
 
 
 describe("Model Point", () => {
-  before(async () => {
+  beforeEach(async () => {
     await dbSync()
   })
 
-  after(async () => {
+  afterEach(async () => {
     await dbDrop()
   })
 
   it("can create points", async () => {
+    await Zone.bulkCreate(zones)
+    await Point.bulkCreate(points)
+    const queryResults = await Point.findAll()
+    expect(queryResults.length)
+      .to.equal(points.length)
+  })
+
+  it("adds zone to points", async () => {
+    const zones = [
+      { trueZoneLabel: "zone3" },
+      { trueZoneLabel: "zone3" },
+      { trueZoneLabel: "zone1" },
+      { trueZoneLabel: "zone1" }
+    ]
+    const expectedStoredPoints = points.map((point, i) => assign(point, zones[i]))
+    await Zone.bulkCreate(zones)
     await Point.bulkCreate(points)
     const queryResults = await Point.findAll()
     const storedPoints = queryResults
-      .map(queryResult => pick(queryResult, keys(points[0])))
+      .map(queryResult => pick(queryResult, keys(expectedStoredPoints[0])))
     expect(sortBy(storedPoints, ["pointId"]))
-      .to.deep.equal(sortBy(points, ["PointId"]))
+      .to.deep.equal(sortBy(expectedStoredPoints, ["PointId"]))
   })
 })
