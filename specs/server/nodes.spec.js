4018e18e5600573b46c200f740ddade84a4ca043
Stefan Dieterle <stefand@artcom.de>
2017-08-29 16:36:29 +0200

    Use restling instead of restler

--- specs/server/nodes.spec.js
+++ specs/server/nodes.spec.js
@@ -1,6 +1,6 @@
 const { describe, it, beforeEach, afterEach } = require("mocha")
 const { expect } = require("chai")
-const restler = require("restler")
+const rest = require("restling")
 const { keys, pick, sortBy } = require("lodash")
 const Node = require("../../src/models/node")
 const nodes = require("../testData/nodes.json")
@@ -21,77 +21,63 @@ describe("Server for nodes", () => {
 
   it("should return all nodes on get at /nodes", async () => {
     await Node.bulkCreate(nodes)
-    restler.get("http://localhost:3000/nodes")
-      .on("complete", (data, response) => {
-        expect(response.statusCode).to.equal(200)
-        expect(sortBy(data, ["id"])).to.deep.equal(sortBy(nodes, ["id"]))
-      })
+    const result = await rest.get("http://localhost:3000/nodes")
+    expect(result.response.statusCode).to.equal(200)
+    expect(sortBy(result.data, ["id"])).to.deep.equal(sortBy(nodes, ["id"]))
   })
 
   it("should return node data on get at /nodes/node-id", async () => {
     await Node.bulkCreate(nodes)
-    restler.get("http://localhost:3000/nodes/20914830ce00")
-      .on("complete", (data, response) => {
-        expect(response.statusCode).to.equal(200)
-        expect(data[0]).to.deep.equal(nodes[1])
-      })
+    const result = await rest.get("http://localhost:3000/nodes/20914830ce00")
+    expect(result.response.statusCode).to.equal(200)
+    expect(result.data[0]).to.deep.equal(nodes[1])
   })
 
   it("should return node id in body and path in location header on single node post at /nodes",
     async () => {
-      restler.post("http://localhost:3000/nodes", { data: nodes[1] })
-        .on("complete", (data, response) => {
-          expect(response.statusCode).to.equal(201)
-          expect(response.headers.location).to.equal("/nodes/20914830ce00")
-          expect(data).to.equal("20914830ce00")
-        })
+      const result = await rest.post("http://localhost:3000/nodes", { data: nodes[1] })
+      expect(result.response.statusCode).to.equal(201)
+      expect(result.response.headers.location).to.equal("/nodes/20914830ce00")
+      expect(result.data).to.equal("20914830ce00")
     }
   )
 
   it("should store the node in the database on single node post at /nodes", async () => {
-    restler.post("http://localhost:3000/nodes", { data: nodes[1] })
-      .on("complete", async (data, response) => {
-        expect(response.statusCode).to.equal(201)
-        const storedNodes = await Node.findAll()
-        expect(pick(storedNodes[0], keys(nodes[1]))).to.deep.equal(nodes[1])
-      })
+    const result = await rest.post("http://localhost:3000/nodes", { data: nodes[1] })
+    expect(result.response.statusCode).to.equal(201)
+    const storedNodes = await Node.findAll()
+    expect(pick(storedNodes[0], keys(nodes[1]))).to.deep.equal(nodes[1])
   })
 
   it("should return node names in body and paths in location header on multiple node post at " +
     "/nodes/bulk",
     async () => {
-      restler.post("http://localhost:3000/nodes/bulk", {
-        data: nodes
-      })
-        .on("complete", (data, response) => {
-          expect(response.statusCode).to.equal(201)
-          expect(response.headers.location).to.equal(
-            "/nodes/20914830f65a; " +
-            "/nodes/20914830ce00; " +
-            "/nodes/6655443322dd; " +
-            "/nodes/665544332211; " +
-            "/nodes/665544332266; " +
-            "/nodes/e0e5cfb58d13"
-          )
-          expect(data).to.deep.equal([
-            "20914830f65a",
-            "20914830ce00",
-            "6655443322dd",
-            "665544332211",
-            "665544332266",
-            "e0e5cfb58d13"
-          ])
-        })
+      const result = await rest.post("http://localhost:3000/nodes/bulk", { data: nodes })
+      expect(result.response.statusCode).to.equal(201)
+      expect(result.response.headers.location).to.equal(
+        "/nodes/20914830f65a; " +
+        "/nodes/20914830ce00; " +
+        "/nodes/6655443322dd; " +
+        "/nodes/665544332211; " +
+        "/nodes/665544332266; " +
+        "/nodes/e0e5cfb58d13"
+      )
+      expect(result.data).to.deep.equal([
+        "20914830f65a",
+        "20914830ce00",
+        "6655443322dd",
+        "665544332211",
+        "665544332266",
+        "e0e5cfb58d13"
+      ])
     }
   )
 
   it("should store the nodes in the database on multiple node post at /nodes/bulk", async () => {
-    restler.post("http://localhost:3000/nodes/bulk", { data: nodes })
-      .on("complete", async (data, response) => {
-        expect(response.statusCode).to.equal(201)
-        const storedNodesQueryResult = await Node.findAll()
-        const storedNodes = storedNodesQueryResult.map(node => pick(node, keys(nodes[0])))
-        expect(storedNodes).to.deep.equal(nodes)
-      })
+    const result = await rest.post("http://localhost:3000/nodes/bulk", { data: nodes })
+    expect(result.response.statusCode).to.equal(201)
+    const storedNodesQueryResult = await Node.findAll()
+    const storedNodes = storedNodesQueryResult.map(node => pick(node, keys(nodes[0])))
+    expect(storedNodes).to.deep.equal(nodes)
   })
 })
