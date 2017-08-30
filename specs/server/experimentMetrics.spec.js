043c22da964fb76096ca80ddf114cddac9ad92cb
Stefan Dieterle <stefand@artcom.de>
2017-08-30 14:33:12 +0200

    Use simplified data for experimentMetrics end point tests

--- specs/server/experimentMetrics.spec.js
+++ specs/server/experimentMetrics.spec.js
@@ -1,11 +1,10 @@
 const { describe, it, beforeEach, afterEach } = require("mocha")
 const { expect } = require("chai")
 const rest = require("restling")
-const { checkPrimaryMetrics } = require("../helpers/data")
 const { dbSync, dbDrop } = require("../helpers/db")
 const ExperimentMetrics = require("../../src/models/experimentMetrics")
-const experimentPrimaryMetrics = require("../testData/experimentPrimaryMetrics.json")
-const points = require("../testData/points.json")
+const experimentPrimaryMetricsSimple = require("../testData/experimentPrimaryMetricsSimple.json")
+const pointsSimple = require("../testData/pointsSimple.json")
 const server = require("../../src/server")
 const { insertExperiment, insertPoints } = require("../../src/storeData")
 const Zone = require("../../src/models/zone")
@@ -17,8 +16,8 @@ describe("Server for primary metrics", () => {
     await dbSync()
     await insertExperiment("test-experiment")
     await Zone.bulkCreate(zones)
-    await insertPoints(points)
-    await ExperimentMetrics.create(experimentPrimaryMetrics)
+    await insertPoints(pointsSimple)
+    await ExperimentMetrics.create(experimentPrimaryMetricsSimple)
     this.server = server.listen(3000, () => console.log("server listening on port 3000"))
   })
 
@@ -34,11 +33,38 @@ describe("Server for primary metrics", () => {
         "http://localhost:3000/experiments/test-experiment/primary-metrics"
       )
       expect(result.response.statusCode).to.equal(200)
-      checkPrimaryMetrics({
-        experimentMetrics: result.data,
-        nonPositionData: true,
-        isQuery: false
-      })
+      checkPrimaryMetrics(result.data)
     }
   )
 })
+
+function checkPrimaryMetrics(metrics) {
+  const errorKeys = [
+    "error2dAverage",
+    "error2dMin",
+    "error2dMax",
+    "error2dVariance",
+    "error2dMedian",
+    "error2dRMS",
+    "error2dPercentile75",
+    "error2dPercentile90",
+    "error3dAverage",
+    "error3dMin",
+    "error3dMax",
+    "error3dVariance",
+    "error3dMedian",
+    "error3dRMS",
+    "error3dPercentile75",
+    "error3dPercentile90",
+    "zoneAccuracyAverage"
+  ]
+  expect(metrics.experimentName).to.equal("test-experiment")
+  for (const key of errorKeys) {
+    expect(metrics[key])
+      .to.be.closeTo(
+      experimentPrimaryMetricsSimple[key],
+      0.0000000000001,
+      `${metrics.experimentName} ${key}`
+    )
+  }
+}
