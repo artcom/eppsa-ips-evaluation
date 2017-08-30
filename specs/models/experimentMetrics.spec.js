32f2545ef7aaaf3930ac1333b05965252ccc4a9d
Stefan Dieterle <stefand@artcom.de>
2017-08-30 10:54:34 +0200

    Simplify test

--- specs/models/experimentMetrics.spec.js
+++ specs/models/experimentMetrics.spec.js
@@ -1,5 +1,7 @@
 const { describe, it, before, after } = require("mocha")
-const { checkPrimaryMetrics } = require("../helpers/data")
+const { expect } = require("chai")
+const { keys, omit } = require("lodash")
+// const { checkPrimaryMetrics } = require("../helpers/data")
 const { dbSync, dbDrop } = require("../helpers/db")
 const experimentPrimaryMetrics = require("../testData/experimentPrimaryMetrics.json")
 const Experiment = require("../../src/models/experiment")
@@ -9,21 +11,31 @@ const ExperimentMetrics = require("../../src/models/experimentMetrics")
 describe("Model ExperimentMetrics", () => {
   before(async () => {
     await dbSync()
+    await Experiment.create({ name: "test-experiment" })
+    await ExperimentMetrics.create(experimentPrimaryMetrics)
   })
 
   after(async () => {
     await dbDrop()
   })
 
-  describe("Model ExperimentMetrics basic function", () => {
-    it("can create experiment metrics", async () => {
-      await Experiment.create({ name: "test-experiment" })
-      await ExperimentMetrics.create(experimentPrimaryMetrics)
-      const experimentMetrics = await ExperimentMetrics.findAll({
-        where: { experimentName: "test-experiment" },
-        include: { model: Experiment }
-      })
-      checkPrimaryMetrics({ experimentMetrics })
+  it("can create experiment metrics", async () => {
+    const experimentMetrics = await ExperimentMetrics.findAll({
+      where: { experimentName: "test-experiment" },
+      include: { model: Experiment }
     })
+    checkPrimaryMetrics(experimentMetrics)
   })
 })
+
+function checkPrimaryMetrics(queryResult) {
+  expect(queryResult[0].experiment.name).to.equal("test-experiment")
+  for (const key of keys(omit(experimentPrimaryMetrics, "experimentName"))) {
+    expect(queryResult[0][key])
+      .to.be.closeTo(
+      experimentPrimaryMetrics[key],
+      0.0000000000001,
+      key
+    )
+  }
+}
