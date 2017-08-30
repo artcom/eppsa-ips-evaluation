11cb6cd1edfd6aaf0aa7cb6b77bb734dc56ba0dc
Stefan Dieterle <stefand@artcom.de>
2017-08-30 16:22:31 +0200

    Rename mockQuuppaDataSimple to mockQuuppaData

--- specs/mocks/getExperimentalData.js
+++ specs/mocks/getExperimentalData.js
@@ -1,14 +1,8 @@
-const data = require("../testData/mockQuuppaData.json")
-const mockQuuppaDataSimple = require("../testData/mockQuuppaDataSimple.json")
+const mockQuuppaData = require("../testData/mockQuuppaData.json")
 
-
-const getMockData = async function getData() {
-  return data
-}
 
 const getData = async function getData() {
-  return mockQuuppaDataSimple
+  return mockQuuppaData
 }
 
-exports.getMockData = getMockData
 exports.getData = getData
