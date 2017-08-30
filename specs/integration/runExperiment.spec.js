e7751e7c159dd5c48391e5bbbc096fd1578419ee
Stefan Dieterle <stefand@artcom.de>
2017-08-30 15:11:34 +0200

    Use simplified data for runExperiment integration tests

--- specs/integration/runExperiment.spec.js
+++ specs/integration/runExperiment.spec.js
@@ -2,22 +2,24 @@ const proxyquire = require("proxyquire")
 const { describe, it, beforeEach, afterEach } = require("mocha")
 const { expect } = require("chai")
 const sinon = require("sinon")
-const { pick } = require("lodash")
-const { checkPositionData, checkPrimaryMetrics } = require("../helpers/data")
+const { concat, keys, pick, sortBy } = require("lodash")
 const { dbDrop } = require("../helpers/db")
 const Experiment = require("../../src/models/experiment")
 const ExperimentMetrics = require("../../src/models/experimentMetrics")
+const experimentPrimaryMetricsSimple = require("../testData/experimentPrimaryMetricsSimple.json")
 const getQuuppaData = require("../../src/getExperimentalData/getQuuppaData")
-const { getMockData } = require("../mocks/getExperimentalData")
+const { getData } = require("../mocks/getExperimentalData")
 const { initializeDb } = require("../../src/initializeDb")
 const { insertPoints, insertExperiment } = require("../../src/storeData")
 const Node = require("../../src/models/node")
 const NodePosition = require("../../src/models/nodePosition")
-const nodePositionsQuuppa = require("../testData/nodePositionsQuuppa.json")
-const nodes = require("../testData/nodes.json")
+const nodePositionsSimple = require("../testData/nodePositionsSimple.json")
+const nodesSimple = require("../testData/nodesSimple.json")
 const Point = require("../../src/models/point")
-const points = require("../testData/points.json")
+const pointsSimple = require("../testData/pointsSimple.json")
+const pointErrorsSimple = require("../testData/pointErrorsSimple.json")
 const PositionData = require("../../src/models/positionData")
+const positionsWithZones = require("../testData/positionsWithZones.json")
 const Zone = require("../../src/models/zone")
 const zones = require("../testData/zones.json")
 
@@ -25,7 +27,7 @@ const zones = require("../testData/zones.json")
 describe("Run a Quuppa experiment", () => {
   beforeEach(async () => {
     await dbDrop()
-    this.getData = sinon.stub(getQuuppaData, "getQuuppaData").callsFake(getMockData)
+    this.getData = sinon.stub(getQuuppaData, "getQuuppaData").callsFake(getData)
     this.runQuuppaExperiment = proxyquire(
       "../../src/runExperiment/quuppaExperiment",
       { getQuuppaData: { getQuuppaData: this.getData } }
@@ -41,7 +43,7 @@ describe("Run a Quuppa experiment", () => {
     it("should set up the database", async () => {
       await initializeDb()
       await Zone.bulkCreate(zones)
-      await insertPoints(points)
+      await insertPoints(pointsSimple)
       const insertedPoints = await Point.findAll()
       const storedPoints = insertedPoints.map(point => pick(point, [
         "name",
@@ -50,7 +52,7 @@ describe("Run a Quuppa experiment", () => {
         "trueCoordinateZ",
         "trueZoneLabel"
       ]))
-      expect(storedPoints).to.deep.equal(points)
+      expect(storedPoints).to.deep.equal(pointsSimple)
     })
   })
 
@@ -59,9 +61,9 @@ describe("Run a Quuppa experiment", () => {
       await initializeDb()
       await insertExperiment("test-experiment")
       await Zone.bulkCreate(zones)
-      await insertPoints(points)
-      await Node.bulkCreate(nodes)
-      await NodePosition.bulkCreate(nodePositionsQuuppa)
+      await insertPoints(pointsSimple)
+      await Node.bulkCreate(nodesSimple)
+      await NodePosition.bulkCreate(nodePositionsSimple)
       await this.runQuuppaExperiment("test-experiment")
       sinon.assert.calledOnce(this.getData)
       await testMetrics()
@@ -74,11 +76,63 @@ async function testMetrics() {
     where: { experimentName: "test-experiment" },
     include: { model: Experiment }
   })
-  await checkPrimaryMetrics({
-    experimentMetrics,
-    experimentName: "test-experiment",
-    nonPositionData: false
-  })
+  checkPrimaryMetrics(experimentMetrics)
   const positionDataQueryResults = await PositionData.findAll()
   await checkPositionData(positionDataQueryResults, false)
 }
+
+function checkPrimaryMetrics(metrics) {
+  const errorKeys = [
+    "error2dAverage",
+    "error2dMin",
+    "error2dMax",
+    "error2dVariance",
+    "error2dMedian",
+    "error2dRMS",
+    "error2dPercentile75",
+    "error2dPercentile90",
+    "error3dAverage",
+    "error3dMin",
+    "error3dMax",
+    "error3dVariance",
+    "error3dMedian",
+    "error3dRMS",
+    "error3dPercentile75",
+    "error3dPercentile90",
+    "zoneAccuracyAverage"
+  ]
+  expect(metrics[0].experimentName).to.equal("test-experiment")
+  for (const key of errorKeys) {
+    expect(metrics[0][key])
+      .to.be.closeTo(
+      experimentPrimaryMetricsSimple[key],
+      0.0000000000001,
+      `${metrics.experimentName} ${key}`
+    )
+  }
+}
+
+function checkPositionData(queryResults) {
+  const errorKeys = ["localizationError2d", "localizationError3d", "zoneAccuracy"]
+  const storedPositionErrors = sortBy(
+    queryResults
+      .map(queryResult => pick(queryResult, concat(errorKeys, "pointName"))),
+    ["pointName"]
+  )
+  expect(
+    sortBy(queryResults, ["pointName"])
+      .map(position => pick(position, keys(positionsWithZones[0])))
+  ).to.deep.equal(positionsWithZones)
+  for (const storedPosition of storedPositionErrors) {
+    const index = storedPositionErrors.indexOf(storedPosition)
+    for (const key of errorKeys) {
+      expect(storedPosition[key])
+        .to.be.closeTo(
+        pointErrorsSimple[index][key],
+        0.00000000000001,
+        key
+      )
+    }
+  }
+}
+
