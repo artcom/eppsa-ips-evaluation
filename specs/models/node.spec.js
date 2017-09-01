a94b50eaa172e5b965d3198b27fad0d155eb05c0
Stefan Dieterle <stefand@artcom.de>
2017-09-01 12:41:28 +0200

    Add unique constraint on node name

--- specs/models/node.spec.js
+++ specs/models/node.spec.js
@@ -1,6 +1,6 @@
 const { describe, it, before, after } = require("mocha")
-const { expect } = require("chai")
-const { keys, pick, sortBy } = require("lodash")
+const { expect, assert } = require("chai")
+const { keys, merge, omit, pick, sortBy } = require("lodash")
 const { dbSync, dbDrop } = require("../helpers/db")
 const Node = require("../../src/models/node")
 const nodes = require("../testData/nodes.json")
@@ -23,6 +23,20 @@ describe("Model Node", () => {
         .map(queryResult => pick(queryResult, keys(nodes[0])))
       expect(sortBy(storedNodes, ["id"]))
         .to.deep.equal(sortBy(nodes, ["id"]))
+    })
+
+    it("raises an error when the node name already exists", async () => {
+      try {
+        await Node.bulkCreate(
+          [
+            nodes[0],
+            merge({ name: "Node1" }, omit(nodes[1], "name"))
+          ]
+        )
+        assert.fail("", "Validation error", "No error was thrown")
+      } catch (error) {
+        expect(error.message).to.equal("Validation error")
+      }
     })
   })
 })
--- src/models/node.js
+++ src/models/node.js
@@ -3,7 +3,7 @@ const db = require("../db")
 
 const Node = db.define("node", {
   id: { type: Sequelize.STRING, allowNull: false, primaryKey: true },
-  name: { type: Sequelize.STRING, allowNull: false },
+  name: { type: Sequelize.STRING, allowNull: false, unique: true },
   type: { type: Sequelize.STRING }
 })
 
