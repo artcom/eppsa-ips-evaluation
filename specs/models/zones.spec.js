8286e0d3534ff8762f05e613a14881a4e29b88cd
Stefan Dieterle <stefand@artcom.de>
2017-09-01 10:15:00 +0200

    Test that points zone labels are recomputed when zones are added

--- specs/models/zones.spec.js
+++ specs/models/zones.spec.js
@@ -2,6 +2,9 @@ const { describe, it, beforeEach, afterEach } = require("mocha")
 const { expect, assert } = require("chai")
 const { keys, pick, sortBy, merge, omit } = require("lodash")
 const { dbSync, dbDrop } = require("../helpers/db")
+const Point = require("../../src/models/point")
+const points = require("../testData/points.json")
+const pointsWithZone = require("../testData/pointsWithZones.json")
 const Zone = require("../../src/models/zone")
 const zones = require("../testData/zones.json")
 
@@ -36,5 +39,15 @@ describe("Model Zone", () => {
     } catch (error) {
       expect(error.message).to.equal("Validation error")
     }
+  })
+
+  it("recomputes trueZoneLabel for points when a new zone is created", async () => {
+    await Point.bulkCreate(points)
+    await Zone.bulkCreate(zones)
+    const queryResults = await Point.findAll()
+    const storedPoints = queryResults
+      .map(queryResult => pick(queryResult, keys(pointsWithZone[0])))
+    expect(sortBy(storedPoints, ["pointId"]))
+      .to.deep.equal(sortBy(pointsWithZone, ["PointId"]))
   })
 })
