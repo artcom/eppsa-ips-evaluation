579610464132f97b61ea0f4bccff672f9a6ef74e
Stefan Dieterle <16118065+globz-eu@users.noreply.github.com>
2017-09-28 16:24:59 +0200

    Add relation to PositionData and ZoneSet

--- specs/models/zoneAccuracy.spec.js
+++ specs/models/zoneAccuracy.spec.js
@@ -1,9 +1,11 @@
 const { describe, it, beforeEach, afterEach } = require("mocha")
 const { expect } = require("chai")
+const { isEqual, keys, pick } = require("lodash")
 const { dbSync, dbDrop } = require("../helpers/db")
 const Node = require("../../src/models/node")
 const nodes = require("../testData/nodes.json")
 const points = require("../testData/points.json")
+const PositionData = require("../../src/models/positionData")
 const positions = require("../testData/positions.json")
 const { insertExperiment, insertPoints, insertPositionData } = require("../../src/storeData")
 const Zone = require("../../src/models/zone")
@@ -35,5 +37,29 @@ describe("Model ZoneAccuracy", () => {
     const zoneAccuracies = storedZoneAccuracies.map(zoneAccuracy => zoneAccuracy.accuracy)
     expect(zoneAccuracies).to.have.length(1)
     expect(zoneAccuracies[0]).to.equal(true)
+  })
+
+  it("has a one to one relationship with PositionData", async () => {
+    const positionData = await PositionData.findById(1)
+    await ZoneAccuracy.create({ accuracy: true, positionDatumId: 1 })
+    const storedZoneAccuracies = await ZoneAccuracy.findAll({
+      where: { positionDatumId: 1 },
+      include: [{ model: PositionData }]
+    })
+    expect(storedZoneAccuracies).to.have.length(1)
+    expect(isEqual(
+      pick(storedZoneAccuracies[0].position_datum, keys(positions[0])),
+      pick(positionData, keys(positions[0]))
+    )).to.equal(true)
+  })
+
+  it("has a one to one relationship with ZoneSet", async () => {
+    await ZoneAccuracy.create({ accuracy: true, zoneSetName: "set1" })
+    const storedZoneAccuracies = await ZoneAccuracy.findAll({
+      include: [{ model: ZoneSet }]
+    })
+    expect(storedZoneAccuracies).to.have.length(1)
+    expect(storedZoneAccuracies[0].zone_set.name)
+      .to.deep.equal("set1")
   })
 })
--- src/models/zoneAccuracy.js
+++ src/models/zoneAccuracy.js
@@ -1,9 +1,14 @@
 const Sequelize = require("sequelize")
 const db = require("../db")
+const PositionData = require("./positionData")
+const ZoneSet = require("./zoneSet")
 
 
 const ZoneAccuracy = db.define("zone_accuracy", {
   accuracy: { type: Sequelize.BOOLEAN, allowNull: false }
 })
+
+ZoneAccuracy.belongsTo(PositionData)
+ZoneAccuracy.belongsTo(ZoneSet)
 
 module.exports = ZoneAccuracy
